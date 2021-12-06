e90fb66044a2c1f74b7e36e3228301da00b28a3b
Marcel Brilha Alves Da Silva <marcel.silva@tokiomarine.com.br>
2021-12-05 21:50:23 -0300

    Modified userService

--- src/main/java/com/final12/final12auth/services/UserService.java
+++ src/main/java/com/final12/final12auth/services/UserService.java
@@ -1,12 +1,46 @@
 package com.final12.final12auth.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import com.final12.final12auth.clients.UserClient;
 import com.final12.final12auth.entities.User;
+import com.final12.final12auth.exceptions.ObjectNotFoundException;
 
-public interface UserService {
+import lombok.extern.slf4j.Slf4j;
 
-	User findByEmail(String email) throws Exception;
+@Service
+@Slf4j
+public class UserService implements UserDetailsService {
 	
-	UserDetails loadUserByUsername(String username) throws Exception;
+	@Autowired
+	private UserClient userClient;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = this.userClient.findByEmail(username).getBody();
+
+		if (user == null) {
+			log.error("Usuário não encontrado: " + username);
+			throw new UsernameNotFoundException("E-mail not found");
+		}
+
+		log.info("Usuário encontrado: " + username);
+		return user;
+	}
+	
+	public User findByEmail(String email) throws Exception {
+		User user = this.userClient.findByEmail(email).getBody();
+		
+		if (user == null) {
+			log.error("E-mail não encontrado: " + email);
+			throw new ObjectNotFoundException("E-mail not found");
+		}
+
+		log.info("E-mail encontrado: " + email);
+		return user;
+	}
 }
